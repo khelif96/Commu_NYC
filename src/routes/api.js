9f4546b98b7f3d221dd0800d5f57aab877152f74
Mohamed Khelif <khelif96@gmail.com>
2017-11-11 11:14:20 -0500

    Created Middlewear to check if an api_token exists in a request

--- src/routes/api.js
+++ src/routes/api.js
@@ -7,7 +7,12 @@ const router = express.Router();
 const authRoute = require('../controller/auth');
 
 router.post('/registerUser', authRoute.registerUser);
+router.post('/loginUser', authRoute.loginUser);
 
+
+// Routes that require an api_token after this
+
+router.use(authRoute.checkAuth);
 
 // Return Router
 module.exports = router;
