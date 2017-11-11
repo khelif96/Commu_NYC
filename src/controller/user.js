b3aa4dd4b8dcee4c47ab53aff6d5e36e41751ed0
Kevin <kevinjia@buffalo.edu>
2017-11-11 12:43:14 -0500

    added user search option

--- src/controller/user.js
+++ src/controller/user.js
@@ -3,16 +3,34 @@ var mongoose = require('mongoose');
 var User = require('../app/models/userSchema');
 
 exports.getUser = (req, res)=>{
-    if(req.body.email == undefined){
+    if (req.body.email == undefined && req.body.user_id == undefined && req.body.api_token == undefined) {
         res.status(400);
-        res.json("No email specified");
-    }else{
-        User.findOne({'email' : req.body.email}, '-password_hash -api_token', function(err, user){
-            if(err){
-                res.json(err);
-            }else{
-                res.json(user);
-            }
-        });
+        res.json("No user specified");
+    } else {
+        if (req.body.email != undefined) {
+            User.findOne({ 'email': req.body.email }, '-password_hash -api_token', function (err, user) {
+                if (err) {
+                    res.json(err);
+                } else {
+                    res.json(user);
+                }
+            });
+        }else if(req.body.user_id != undefined){
+            User.findOne({ '_id': req.body.user_id }, '-password_hash -api_token', function (err, user) {
+                if (err) {
+                    res.json(err);
+                } else {
+                    res.json(user);
+                }
+            });
+        }else if(req.body.api_token != undefined){
+            User.findOne({ 'api_token': req.body.api_token }, '-password_hash -api_token', function (err, user) {
+                if (err) {
+                    res.json(err);
+                } else {
+                    res.json(user);
+                }
+            });
+        }
     }
 }
