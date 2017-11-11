09db30354e7d9f61cee34cbfb22b25102a5a5352
Kevin <kevinjia@buffalo.edu>
2017-11-11 12:55:12 -0500

    included options to search for users

--- src/controller/user.js
+++ src/controller/user.js
@@ -2,35 +2,47 @@ var mongoose = require('mongoose');
 
 var User = require('../app/models/userSchema');
 
-exports.getUser = (req, res)=>{
-    if (req.body.email == undefined && req.body.user_id == undefined && req.body.api_token == undefined) {
+exports.getUserbyEmail = (req, res) => {
+    if (req.body.email == undefined) {
         res.status(400);
-        res.json("No user specified");
+        res.json("No email specified");
     } else {
-        if (req.body.email != undefined) {
-            User.findOne({ 'email': req.body.email }, '-password_hash -api_token', function (err, user) {
-                if (err) {
-                    res.json(err);
-                } else {
-                    res.json(user);
-                }
-            });
-        }else if(req.body.user_id != undefined){
-            User.findOne({ '_id': req.body.user_id }, '-password_hash -api_token', function (err, user) {
-                if (err) {
-                    res.json(err);
-                } else {
-                    res.json(user);
-                }
-            });
-        }else if(req.body.api_token != undefined){
-            User.findOne({ 'api_token': req.body.api_token }, '-password_hash -api_token', function (err, user) {
-                if (err) {
-                    res.json(err);
-                } else {
-                    res.json(user);
-                }
-            });
-        }
+        User.findOne({ 'email': req.body.email }, '-password_hash -api_token', function (err, user) {
+            if (err) {
+                res.json(err);
+            } else {
+                res.json(user);
+            }
+        });
+    }
+}
+
+exports.getUserbyID = (req, res) => {
+    if (req.body.user_id == undefined) {
+        res.status(400);
+        res.json("No user ID specified");
+    } else {
+        User.findOne({ '_id': req.body.user_id }, '-password_hash -api_token', function (err, user) {
+            if (err) {
+                res.json(err);
+            } else {
+                res.json(user);
+            }
+        });
+    }
+}
+
+exports.getUserbyAPI = (req, res) => {
+    if (req.body.api_token == undefined) {
+        res.status(400);
+        res.json("No API token specified");
+    } else {
+        User.findOne({ 'api_token': req.body.api_token }, '-password_hash -api_token', function (err, user) {
+            if (err) {
+                res.json(err);
+            } else {
+                res.json(user);
+            }
+        });
     }
 }
--- src/routes/api.js
+++ src/routes/api.js
@@ -9,7 +9,9 @@ const userInfo = require('../controller/user');
 
 router.post('/registerUser', authRoute.registerUser);
 router.post('/loginUser', authRoute.loginUser);
-router.post('/userInfo', userInfo.getUser);
+router.post('/userInfobyEmail', userInfo.getUserbyEmail);
+router.post('/userInfobyID', userInfo.getUserbyID);
+router.post('/userInfobyAPI', userInfo.getUserbyAPI);
 
 // Routes that require an api_token after this
 
