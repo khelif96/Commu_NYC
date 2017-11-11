ffa55a11a833f7980e3168c6820763cb26d7e954
tanight <ohitzfrankie@gmail.com>
2017-11-11 18:53:25 -0500

    post example

--- commu-nyc/src/Scenes/PostList.js
+++ commu-nyc/src/Scenes/PostList.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import '../Styles/App.css';
+import {PanelGroup ,Panel} from 'react-bootstrap';
+import JobPanel from './Feed/JobPanel'
+import { JobHeading,FeedContainer,Organization,DatePosted,Description } from '../Styles/feed.style';
 
 class PostList extends Component {
 
@@ -8,16 +11,41 @@ class PostList extends Component {
   }
 
   render() {
-    var styleBigHeader =  {
-        color : 'orange',
-        fontSize : 100,
-        textAlign : 'center',
-        align : 'center'
+    var pageStyle = {
+        fontSize : 50,
     }
+    var pageHeader = {
+        color : 'orange',
+        fontSize : 50
+    }
+
     return (
-          <div style = {styleBigHeader}>
-            <button style = {styleBigHeader}>Job Posting</button>
+
+        <div >
+          <div style = {pageHeader}> Post List </div>
+          <div style = {pageStyle}> 
+            <a href = "/PostPage">Job 1</a>
           </div>
+          <div style = {pageStyle}> 
+            <a href = "/PostPage">Job 1</a>
+          </div>
+          <div style = {pageStyle}> 
+            <a href = "/PostPage">Job 1</a>
+          </div>
+          <div style = {pageStyle}> 
+            <a href = "/PostPage">Job 1</a>
+          </div>
+          <div style = {pageStyle}> 
+            <a href = "/PostPage">Job 1</a>
+          </div>
+          <div style = {pageStyle}> 
+            <a href = "/PostPage">Job 1</a>
+          </div>
+          <div style = {pageStyle}> 
+            <a href = "/PostPage">Job 1</a>
+          </div>
+
+        </div>
     );
   }
 }
