5a1bb0cf20843d80bf6c6fcd4c9cc7540f8cac6d
Kevin <kevinjia@buffalo.edu>
2017-11-11 20:25:20 -0500

    optimized getOpportunitiesbyVolunteer

--- src/controller/getOpportunities.js
+++ src/controller/getOpportunities.js
@@ -1,28 +1,38 @@
 var Opportunity = require('../app/models/opportunitySchema');
+var User = require('../app/models/userSchema');
 
 exports.getOpportunities = (req, res) => {
-    Opportunity.find({}, function(err, opportunities){
-        err? res.json(err) : res.send(opportunities);
+    Opportunity.find({}, function (err, opportunities) {
+        err ? res.json(err) : res.send(opportunities);
     });
 }
 
 exports.getOpportunitybyID = (req, res) => {
-    Opportunity.findOne({"_id" : req.body.opportunity_id}, function(err, opportunity){
-        err? res.json(err) : res.send(opportunity);
+    Opportunity.findOne({ "_id": req.body.opportunity_id }, function (err, opportunity) {
+        err ? res.json(err) : res.send(opportunity);
     });
 }
 
 exports.getOpportunitiesbyVolunteer = (req, res) => {
-    var opportunitiesSignedUp = [];
-    Opportunity.find({}, function(err, opportunities){
-        for(var i = 0; i < opportunities.length; i++){
-            for(var j = 0; j < opportunities[i].volunteerIds.length; j++){
-                if(opportunities[i].volunteerIds[j] == req.body.volunteer_id){
-                    opportunitiesSignedUp.push(opportunities[i]);
-                    break;
+    promise = new Promise(function (resolve, reject) {
+        var opportunitiesSignedUp = [];
+        // Find user in database and get their list of events they're signed up for
+        User.findById({ "_id": req.body.volunteer_id }, function (err, user) {
+            if (err) res.json(err)
+            else {
+                if (user.eventsHelped.length == 0) res.json("User is not signed up for any event");
+                for (var i = 0; i < user.eventsHelped.length; i++) {
+                    Opportunity.findOne({ "_id": user.eventsHelped[i] }, function (err, event) {
+                        err ? res.json(err) : opportunitiesSignedUp.push(event);
+                        if(opportunitiesSignedUp.length == user.eventsHelped.length){
+                            resolve(opportunitiesSignedUp);
+                        }
+                    })
                 }
             }
-        }
-        res.send(opportunitiesSignedUp);
-    });
+        });
+    })
+    promise.then(function(result){
+        res.send(result);
+    })
 }
